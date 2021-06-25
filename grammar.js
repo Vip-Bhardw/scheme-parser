6b7f8ab65553b88b7673adeaf86ee7799d5a770a
Vip-Bhardw <vipulbhardwaj1011@gmail.com>
2021-06-25 22:25:27 +0530

    included error logs

--- grammar.js
+++ grammar.js
@@ -1,18 +1,10 @@
 module.exports = grammar({
     name: 'Scheme',
-
-    // rules: {
-    //     //add grammar rules here...
-    //     sourceFile: $ => '123',
-
-    // }
-
     rules: {
-        source_file: $ => repeat(choice($.number,$.alphabets)),    
-        
+        source_file: $ => repeat(choice($.number,$.alphabets)),
+
         alphabets: $ => /[a-z]+/,
-    
+
         number: $ => /\d+/
       }
-    
 });
